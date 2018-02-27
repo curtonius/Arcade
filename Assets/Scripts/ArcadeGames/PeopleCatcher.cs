4cdda60df59fd6710aac6d625d2dd3a7482b9e3f
Curtonius <cmontphillips@yahoo.com>
2018-02-26 17:53:05 -0800

    Added PeopleCatcher game

---
+++ Assets/Scripts/ArcadeGames/PeopleCatcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleCatcher : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Rigidbody>())
+        {
+            other.GetComponent<Rigidbody>().velocity = new Vector3(0, Random.Range(5,15), 0);
+            other.GetComponent<Rigidbody>().useGravity = true;
+        }
+        /*Jump jump = other.GetComponentInParent<Jump>();
+        if (jump)
+        {
+            Debug.Log("JUMP");
+            jump.jumpHeight += Random.Range(0, 10);
+            jump.JumpUp();
+        }*/
+    }
+}
